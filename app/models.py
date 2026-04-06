40b687733e68dea57fa6c9a5093446422b03b4cb
Bruvm <melibruvera@hotmail.com>
2026-04-05 22:52:11 -0300

    Primer commit

---
+++ app/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+class Role(models.Model):
+    name = models.CharField(
+        max_length=50,
+        unique=True,
+        help_text="Nombre del rol, debe ser único"
+    )
+    description = models.TextField(
+        blank=True,
+        help_text="Descripción del rol" 
+    )
+
+    def __str__(self):
+        return self.name
+
+class User(models.Model):
+    name = models.CharField(
+        max_length=100,
+        help_text="Nombre completo del usuario"
+    )
+    email = models.EmailField(
+        unique=True,
+        help_text="Correo electrónico único"  
+    )
+    dni = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^\d{8}[A-Za-z]$'
+            )
+        ],
+        help_text="Número de documento(7 u 8 dígitos)"
+    )
+    birth_date = models.DateField(
+        help_text="Fecha de nacimiento del usuario"
+    )
+    role = models.ForeignKey(
+        Role, on_delete=models.CASCADE,
+        related_name='users',
+    )
+    activo = models.BooleanField(
+        default=True,
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.role})"
---
+++ app/views.py
@@ -0,0 +1,49 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView as ApiView
+
+from app.models import User, Role
+from app.serializers import UserSerializer, RoleSerializer
+
+class UserView(ApiView):
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+
+    def delete(self, request):
+        User.objects.all().delete()
+        return Response(status=204) 
+    
+    def put(self, request):
+        user_id = request.data.get('id')
+        try:
+            user = User.objects.get(id=user_id)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found'}, status=404)
+        
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=400)
+
+class RoleView(ApiView):
+    def get(self, request):
+        roles = Role.objects.all()
+        serializer = RoleSerializer(roles, many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = RoleSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
+# Create your views here.
